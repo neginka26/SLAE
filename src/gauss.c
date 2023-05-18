deb970ec06e7b27515638ed56690c47d584f7190
neginka_26 <hei-i-iuhka@mail.ru>
2023-05-18 22:05:20 +0300

    добавлена обратная проходка

--- src/gauss.c
+++ src/gauss.c
@@ -3,34 +3,35 @@
 
 float **create_matrix(int m, int n);
 void fill_matrix(float **matrix, int m, int n);
-void output_matrix(float **matrix, int m, int n);
+void output_matrix_with_right_part(float **matrix, int m, int n);
 void free_matrix(float **matrix, int m);
-void hilbert_matrix_5_order(float **hilbert_matrix);
-void right_part(float **right_part);
+
 float find_determ_of_matrix(float **matrix, int dim);
 void remove_line_and_column(float **matrix, int dim, float **result_matrix,
                             int line, int column);
 int check_for_triangular_matrix(float determ);
-void lead_to_triangular(float **matrix, int dim);
+void lead_to_triangular(float **matrix, int dim, int dim_1);
+float* reverse_motion(float **matrix, int dim, int dim_1);
+
+void output_array(float *array, int dim);
 
 int main() {
-  int dimension = 3;
-  // выделение памяти под основную матрицу и правую часть
-  float **matrix = create_matrix(dimension, dimension);
-  float **right_part = create_matrix(dimension, 1);
-  // заполнение матрицы
-  fill_matrix(matrix, dimension, dimension);
-  fill_matrix(matrix, dimension, 1);
-  // вывод матрицы и правой части
-  output_matrix(matrix, dimension, dimension);
-  output_matrix(right_part, dimension, 1);
-  // проверка: матрица вырожденная (определитель = 0)?
-  if (check_for_triangular_matrix(find_determ_of_matrix(matrix, dimension))) {
-    lead_to_triangular(matrix, dimension);
-    output_matrix(matrix, dimension, dimension);
+  int dim = 3;
+  float **matrix = create_matrix(dim, dim + 1);
+  fill_matrix(matrix, dim, dim + 1);
+  printf("Введена матрица:\n");
+  output_matrix_with_right_part(matrix, dim, dim + 1);
+  float determ = find_determ_of_matrix(matrix, dim);
+  printf("Определитель:\n%f\n", determ);
+
+  if (check_for_triangular_matrix(find_determ_of_matrix(matrix, dim))) {
+    lead_to_triangular(matrix, dim, dim + 1);
+    output_matrix_with_right_part(matrix, dim, dim + 1);
+    float *x = reverse_motion(matrix, dim, dim+1);
   } else {
     printf("Матрица невырожденная");
   }
+  free_matrix(matrix, dim);
   return 0;
 }
 
@@ -53,15 +54,15 @@ void fill_matrix(float **matrix, int m, int n) {
 }
 
 // Вывод матрицы
-void output_matrix(float **matrix, int m, int n) {
+void output_matrix_with_right_part(float **matrix, int m, int n) {
   for (int i = 0; i < m; i++) {
     for (int j = 0; j < n; j++) {
       if (j < n - 1) {
         printf("%f ", matrix[i][j]);
       } else if (j == n - 1 && i != m - 1) {
-        printf("%f\n", matrix[i][j]);
+        printf(" | %f\n", matrix[i][j]);
       } else {
-        printf("%f", matrix[i][j]);
+        printf("| %f", matrix[i][j]);
       }
     }
   }
@@ -74,27 +75,6 @@ void free_matrix(float **matrix, int m) {
     free(matrix[i]);
   }
   free(matrix);
-}
-
-// Заполнение матрицы как матрица Гильберта 5 порядка
-void hilbert_matrix_5_order(float **hilbert_matrix) {
-  int dim = 5;
-  for (int i = 0; i < dim; i++) {
-    for (int j = 0; j < dim; j++) {
-      hilbert_matrix[i][j] = 1.0 / ((i + 1) + (j + 1) - 1);
-    }
-  }
-}
-
-// Заполнение матрицы правой части
-void right_part(float **right_part) {
-  int column = 0;
-  int line = 5;
-  int counter = 1;
-  for (int i = 0; i < line; i++) {
-    right_part[i][column] = counter;
-    counter++;
-  }
 }
 
 // нахождение определителя матрицы рекурсивным методом
@@ -152,22 +132,39 @@ int check_for_triangular_matrix(float determ) {
 }
 
 // приведение к треугольному виду (метод Гаусса)
-void lead_to_triangular(float **matrix, int dim) {
+void lead_to_triangular(float **matrix, int dim, int dim_1) {
   float r = 0;
   for (int k = 0; k < dim; k++) {
     for (int i = k + 1; i < dim; i++) {
       r = matrix[i][k] / matrix[k][k];
-      for (int j = k; j < dim; j++) {
+      for (int j = k; j < dim_1; j++) {
         matrix[i][j] = matrix[i][j] - r * matrix[k][j];
       }
     }
   }
 }
 
-// минусы: нет проверки деления на нуль в момент приведения к треугольному виду
-// (при подборе коэффициента r не учитывается, что текущим элементом может быть
-// нуль). Для случая с нулем нужно предусмотреть функцию смены строк местами.
+// обратный ход и нахождение X-ов
+float* reverse_motion(float **matrix, int dim, int dim_1) {
+    float *answers = (float*)malloc(dim * sizeof(float));
+    int ind = dim_1 - 1;
+    float x = 0;
+    int diag_ind = dim - 1;
+    for (int i = dim-1; i >= 0; i--) {
+        x = matrix[i][ind];
+        for (int j = dim-1; j > i; j--) {
+            x -= matrix[i][j] * answers[i+1]; 
+        }
+        answers[i] = x / matrix[i][diag_ind];
+        diag_ind--;
+    }
+    return answers;
+}
 
-// to do: 1) совместить преобразование к трегольному виду с правой частью уравнений
-// - complete
-// 2) сделать решение СЛАУ (обратный ход)
+// вывод массива (одномерного)
+void output_array(float *array, int dim) {
+    for (int i = 0; i < dim; i++) {
+        if (i < dim - 1) printf("%f ", array[i]);
+        else printf("%f\n", array[i]);
+    }
+}
