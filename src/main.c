76671b6a080767960a16ac96bde1f4fef9339da8
neginka26 <hei-i-iuhka@mail.ru>
2023-05-28 15:35:28 +0300

    Приведение к треугольному виду

--- src/main.c
+++ src/main.c
@@ -1,36 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 typedef struct Num {
-  int numerator;
-  int denominator;
+  long long numerator;
+  long long denominator;
 } num;
 
 num **create_matrix(int m, int n);
 void hilbert_matrix_5_order(num **hilbert_matrix);
 void output_matrix_with_right_part(num **matrix, int m, int n);
 void free_matrix(num **matrix, int m);
-int check_for_triangular_matrix(float determ);
 void lead_to_triangular(num **matrix, int dim, int dim_1);
 num find_determ_of_matrix(num **matrix, int dim);
 float get_float_num(num n);
+
 num division_fractions(num n_1, num n_2);
 num product_fractions(num n_1, num n_2);
 num sum_fractions(num n_1, num n_2);
 num difference_fractions(num n_1, num n_2);
 
+long long find_greatest_common_divisor(num n);
+num shorten_fraction(num n);
+
 int main() {
   int dim = 5;
   num **matrix = create_matrix(dim, dim + 1);
+  num **matrix_1 = create_matrix(dim, dim + 1);
+  num determ = {1, 1};
+
   hilbert_matrix_5_order(matrix);
+  hilbert_matrix_5_order(matrix_1);
+
   printf("Введена матрица:\n");
   output_matrix_with_right_part(matrix, dim, dim+1);
-  lead_to_triangular(matrix, dim, dim+1);
-  printf("Треугольная матрица:\n");
-  output_matrix_with_right_part(matrix, dim, dim+1);
-//   if () {
 
-//   }
+  lead_to_triangular(matrix, dim, dim+1);
+  printf("\nТреугольная матрица:\n");
+  output_matrix_with_right_part(matrix, dim, dim+1);
+
+  determ = find_determ_of_matrix(matrix, dim);
+  printf("\nОпределитель: %lld/%lld\n", determ.numerator, determ.denominator);
+
+  if (get_float_num(determ) != 0) {
+    printf("\nМетод Гаусса с выбором главного элемента:\n");
+    output_matrix_with_right_part(matrix_1, dim, dim + 1);
+  } else {
+    printf("Матрица невырожденная\n");
+  }
+
+  free_matrix(matrix_1, dim);
   free_matrix(matrix, dim);
 
   return 0;
@@ -92,28 +111,18 @@ void free_matrix(num **matrix, int m) {
   free(matrix);
 }
 
-//проверка на возможность приведения матрицы к треугольному виду
-int check_for_triangular_matrix(float determ) {
-  int is_triang = 0;
-  if (determ == 0)
-    is_triang = 0;
-  else
-    is_triang = 1;
-  return is_triang;
-}
-
 // приведение к треугольному виду (метод Гаусса)
 void lead_to_triangular(num **matrix, int dim, int dim_1) {
   num r = {1,1};
   for (int k = 0; k < dim; k++) {
     for (int i = k + 1; i < dim; i++) {
       r = division_fractions(matrix[i][k], matrix[k][k]); // matrix[i][k] / matrix[k][k]
+      r = shorten_fraction(r);
       for (int j = k; j < dim_1; j++) {
         matrix[i][j] = difference_fractions(matrix[i][j], product_fractions(r, matrix[k][j]));  //matrix[i][j] - r * matrix[k][j];
+        matrix[i][j] = shorten_fraction(matrix[i][j]);
       }
     }
-    printf("\nМатрица:\n");
-    output_matrix_with_right_part(matrix, dim, dim_1);
   }
 }
 
@@ -121,6 +130,7 @@ num division_fractions(num n_1, num n_2) {
   num res = {0, 1};
   res.numerator = n_1.numerator * n_2.denominator;
   res.denominator = n_1.denominator * n_2.numerator;
+  res = shorten_fraction(res);
   return res;
 }
 
@@ -145,6 +155,7 @@ num sum_fractions(num n_1, num n_2) {
     res.numerator = n_1.numerator + n_2.numerator;
     res.denominator = n_1.denominator;
   }
+  res = shorten_fraction(res);
   return res;
 }
 
@@ -163,4 +174,28 @@ num difference_fractions(num n_1, num n_2) {
     res.denominator = n_1.denominator;
   }
   return res;
+}
+
+long long find_greatest_common_divisor(num n) {
+    long long gcd = 1;
+    while (n.numerator != n.denominator) {
+        if (n.numerator > n.denominator) n.numerator -= n.denominator;
+        else if (n.numerator == 0) break;
+        else n.denominator -= n.numerator;
+    }
+    gcd = n.denominator;
+    return gcd;
+}
+
+num shorten_fraction(num n) {
+    long long gcd = find_greatest_common_divisor(n); 
+    n.numerator /= gcd;
+    n.denominator /= gcd;
+    return n;
+}
+
+num find_determ_of_matrix(num **matrix, int dim) {
+  num determ = {1, 1};
+  for (int i = 0; i < dim; i++) determ = product_fractions(determ, matrix[i][i]);
+  return determ;
 }
